c472e8a2006d87006831f85472275aaf3a493f13
Numan <beautykhan832@gmail.com>
2025-07-18 03:42:37 +0600

    firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXQgSge6v3tOu-GJtpPqhXwzg2n2nmO2s",
+  authDomain: "kmnuman-a9daa.firebaseapp.com",
+  projectId: "kmnuman-a9daa",
+  storageBucket: "kmnuman-a9daa.appspot.com",
+  messagingSenderId: "132530675488",
+  appId: "1:132530675488:web:exampleappid"
+};
+firebase.initializeApp(firebaseConfig);
